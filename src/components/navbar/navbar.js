1e1497c422578d29f10666f88196b4845e387073
bondanr rahadian <bondanrp@gmail.com>
2021-07-16 05:08:00 +0700

    trying to deploy

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,8 +5,8 @@ import "./navbar.scss";
 export default function Navbar() {
   return (
     <div className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/my-pokemon">My Pokemon</Link>
+      <Link to="./">Home</Link>
+      <Link to="./my-pokemon">My Pokemon</Link>
     </div>
   );
 }
